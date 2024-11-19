0ef236fca0515dde16ceff167f0e5f9a7765a15e
snehaanap <anapsneha@gmail.com>
2024-11-19 20:40:30 +0530

    commit

---
+++ Batch3_students/SNEHA_SOURCE/ASSIGNMENT/js_assignment/01_sneha.js
@@ -0,0 +1,77 @@
+// Write a single line comment which says, comments can make code readable
+// comments can make code readable
+
+//Write another single comment which says, Welcome to 30DaysOfJavaScript
+//Welcome to 30DaysOfJavaScript
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+//comments can make code readable
+
+// easy to reuse and informative
+
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+// String
+let Name1="sneha"
+
+//Number
+let AGE2 = 25;
+
+//boolean
+let isLoggedof1 = true;
+
+//undefined
+let anap12;
+
+//null
+let Mark1 = null;
+
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+// String
+let Name="sneha"
+console.log("Data type of Name",typeof(Name));
+//Number
+let AGE = 25;
+console.log("Data type of AGE",typeof(AGE));
+//boolean
+let isLoggedof = true;
+console.log("Data type of isLoggedof",typeof(isLoggedof));
+//undefined
+let anap;
+console.log("Data type of anap",typeof(JADHAV));
+//null
+let Mark = null;
+console.log("Data type of Mark",Mark);
+
+// Q no.6 Declare four variables without assigning values
+let firstname;
+let lastename;
+let age;
+let city;
+
+
+// Declare four variables with assigned values
+let Firstname = "sneha";
+let Lastename = "anap";
+let Age ='25';
+let City ="sangamner";
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let first_name = "sneha";
+let last_name = "anap";
+let marital_status = "married";
+let country = "sangamner";
+let age1 = "25"
+console.log(first_name);
+console.log(last_name);
+console.log(marital_status);
+console.log(country);
+console.log(age1);
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+console.log("Firstname"+ first_name, "Lastname"+ last_name,"Maritalstatus"+ marital_status,"Country"+ country,"Age"+ age1);
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the console.
+let myAge = 25;
+let yourAge = 30;
+console.log(myAge)
+console.log(yourAge)

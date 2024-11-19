5a597e9b73f025a46be3cb0536932f4c9a7aabb4
Tejas-Pund <tejasspund@gmail.com>
2024-11-19 21:23:58 +0530

    js assignment

---
+++ Batch3_students/Tejas/Assignments/Assignment1.js
@@ -0,0 +1,36 @@
+// This is a single line comment
+// welcome to 360 days of javascript
+/* 
+hello
+this is a multiple line comment
+*/
+/*
+comments can make code readable
+easy to reuse and informative
+*/
+
+let w
+console.log(w)
+let x
+console.log(x)
+let y
+console.log(y)
+let z
+console.log(z)
+let a = 1
+console.log(a)
+let b = 2
+console.log(b)
+let c = 3
+console.log(c)
+let d = 4
+console.log(d)
+let first_name = "Tejas"
+let last_name = "Pund"
+let age = 29
+let marital_status = "Married"
+console.log(first_name)
+console.log(last_name)
+console.log(age)
+console.log(marital_status)
+console.log(first_name, last_name, age, marital_status)

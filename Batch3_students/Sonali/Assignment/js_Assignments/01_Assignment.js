4d4e265767a9111798614c0a0c00a2ec0d02d382
SonuKhosle <sonukhosle@gmail.com>
2024-11-19 17:15:42 +0530

    Assignment completed

--- Batch3_students/Sonali/Assignment/js_Assignments/01_Assignment.js
+++ Batch3_students/Sonali/Assignment/js_Assignments/01_Assignment.js
@@ -1,19 +1,62 @@
 // Write a single line comment which says, comments can make code readable
+//comments can make code readable
+
 
 // Write another single comment which says, Welcome to 30DaysOfJavaScript
+//Welcome to 30Days  Of JavaScript
+
 
 // Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/**
+ * Welcome to 30 Days Of Javascript
+ * Comments can make code readable,
+ * easy to reuse
+ * and informative
+ */
 
 // Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let name="Sonali"
+let x=true
+let y= undefined
+let num= "null"
+
 
 // Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+let a=10
+console.log(typeof(a))
+
+let str="Sonali"
+console.log(typeof(str))
+
+var x1=true
+console.log(typeof(x1))
 
 // Declare four variables without assigning values
+let Name
+var age
+const firstname="khosle"
+
 
 // Declare four variables with assigned values
+let a1=24;
+
+var name1="Sonali"
+
+const pi=3.14
 
 // Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName= "Sonali"
+let lastName= "Khosle"
+let maritalStatus= "Single"
+let country= "India"
+let age1= 24
 
 // Declare variables to store your first name, last name, marital status, country and age in a single line
+let firstName1= "Sonali", lastName2= "Khosle", maritalStatus1= "Single", country1= "India", age2= "24"
+
 
 // Declare two variables myAge and yourAge and assign them initial values and log to the console.
+let myAge= 24
+let yourAge= 30
+
+console.log(myAge , yourAge)

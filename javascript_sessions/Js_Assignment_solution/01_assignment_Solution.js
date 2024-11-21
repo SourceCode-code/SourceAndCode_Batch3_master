ba50c4c937318bade45b25e4714c12192800e578
KingThomas <akshay.kshirsagar2942@gmaiil.com>
2024-11-21 12:17:21 +0530

    new

---
+++ javascript_sessions/Js_Assignment_solution/01_assignment_Solution.js
@@ -0,0 +1,45 @@
+// Write a single line comment which says, comments can make code readable
+//comments can make code readable
+
+// Write another single comment which says, Welcome to 30DaysOfJavaScript
+
+// Welcome to 30DaysOfJavaScript
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+/**
+ * comments can make code readable,
+ *  easy to reuse and informative
+ */
+
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+// Declare four variables without assigning values
+
+let a
+let b
+let c
+let d
+
+// Declare four variables with assigned values
+let aa = 1
+let bb = 2
+let cc = 3
+let dd = 4
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstname = "siddhant"
+let lastname = "gadakh"
+let maritalstatus = "umnarried"
+let country = "india"
+let age = 25
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let firstname1 = "siddhant", lastname1 = "gadakh", maritalstatus1 = "umnarried", country1 = "india", age1 = 25
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the console.
+let myAge = 24
+let yourAge = 25
+

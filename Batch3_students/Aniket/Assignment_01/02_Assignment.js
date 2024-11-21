f532d0ed3577538202086c847adade8023d4a331
aniket <aniketskadam50gmail.com>
2024-11-21 21:58:44 +0530

    01_Assignment

--- Batch3_students/Aniket/Assignment_01/02_Assignment.js
+++ Batch3_students/Aniket/Assignment_01/02_Assignment.js
@@ -5,8 +5,8 @@
 // Simple Questions:
 
 // 1. What will be the output of the following code? 
- let x = 7.2;
- console.log(Math.round(x));
+  let x = 7.2;
+  console.log(Math.round(x));
 // // Hint: It will round to the nearest integer. write in comment
 
 // 2. Write code to check the type of the variable `gravity = 9.81` using the typeof operator.
@@ -18,5 +18,11 @@
 
 // 4. What is the output of rounding the number 5.7 using Math.ceil() and Math.floor()? 
 //  let num = 5.7;
+
+let num1=5.8
+console.log(Math.ceil(num1))
+
+let num2=9.9999
+console.log(Math.floor(num2))
 // Hint: Rounds up to the nearest integer.
 // Hint: Rounds down to the nearest integer.

bec2f6918c9f989c8636df8e27a79bf99bcd15a8
pavanbhoyate <bhoyatepavan3@gmail.com>
2024-11-25 19:54:16 +0530

    assignment3

---
+++ Batch3_students/pavan/assignment/03_Assignment.js
@@ -0,0 +1,23 @@
+// generate a random number between 1 to 10 and print it//
+
+console.log(Math.floor(Math.random()*9)+1)
+
+
+// what is the output of rounding the number 5.7 using Math.ceil() and Math.floor()//
+
+let a = 5.7
+console.log(Math.ceil(a))
+console.log(Math.floor(a))
+
+
+// write program rolldice that stimulate rolling two dice and return their combined result//
+
+console.log(Math.floor(Math.random()*5)+1)
+
+
+// generate random between 10 to 20 should generate number between 10 to 20//
+
+console.log(Math.floor(Math.random()*19)+1)
+
+
+

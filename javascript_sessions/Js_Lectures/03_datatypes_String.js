fd70eb648da29166560c501d1a40cb33f7cc1321
KingThomas <akshay.kshirsagar2942@gmaiil.com>
2024-11-26 22:49:57 +0530

    string

---
+++ javascript_sessions/Js_Lectures/03_datatypes_String.js
@@ -0,0 +1,54 @@
+// datatypes in js 
+
+// primitive  --> primtive are predefiend they are im mutable  (number ,string)
+// non primitive --> nonprimtive are not  predefiend they are  mutable
+
+
+//1 string--> string is a sequence of charcter that can be idenfied by doublequotes(""),single qutoes(''),backticks(``)
+
+let str = ""               //string 
+let str1 = "hello " //sgtring
+let str3 ='724568752'  // string // 
+let str4= `!@#$%` // string
+let str5 = 'true' //string
+let str6 = "                "//string
+
+console.log(typeof str)
+console.log(typeof str1)
+
+console.log(typeof str3)
+console.log(typeof str4)
+console.log(typeof str5)
+console.log(typeof str6)
+
+
+let num = 8567
+
+// string basics -->
+// length -->
+let str7="siddhant "  //8
+
+// how to check length of a string 
+
+console.log(str7.length)
+
+
+// s  i  d  d  h  a  n  t 
+// 0  1  2  3  4  5  6  7 8 
+
+// srting stores the variables in indexes
+
+console.log(str7.length-1)  // is always the last index 
+
+// to retrive any charcter 
+
+console.log(str7[1]) //i
+
+console.log(str7[str7.length-1]) //t
+
+console.log(str7)
+
+
+
+
+

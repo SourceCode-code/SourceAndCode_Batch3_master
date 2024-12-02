2ab6fe183fdbb034752bd8959453c1fe895613ea
Ashwini Tanaji Kadam <ashwinit.kadam@expleogroup.com>
2024-12-03 00:09:10 +0530

    )3 assignment

--- Batch3_students/Ashwini/Assignments/JS_Assignments/01Assignments/03assignment.js
+++ Batch3_students/Ashwini/Assignments/JS_Assignments/01Assignments/03assignment.js
@@ -1,28 +1,40 @@
 // Simple Session Assignments:
 
 // 1. Declare a string variable `greeting` with the value "Hello World" and log its data type.
-let greeting="Hello World"
-console.log(greeting)
+let greeting = "Hello World"
+console.log(typeof greeting) // string
 
 // 2. Declare a number variable `price` with a value of 199.99, then use Math.ceil() to round it up to the nearest whole number and log the result.
-
+const price = 199.99
+console.log(Math.ceil(price))
 
 // 3. Create a string `name` with your full name and log the length of the string using the `.length` property.
-
+let name = "Ashwini"
+console.log("String length :" + name.length)
 
 // 4. Write a program that concatenates three string variables `firstName`, `middleName`, and `lastName` using both the `+` operator and template literals, then log the full name.
-
-
+let fn = "Ashwini"
+let mn = "Tanaji"
+let ln = "Kadam"
+console.log("Full name :" + fn + " " + mn + " " + ln + " ")
+console.log("Full name :" + fn.concat(mn, ln))
 
 // 5. Given a string `language = 'JavaScript'`, log the character at the 4th index in the string.
+let language = "Javascript"
 
+console.log("4th index :" + language.indexOf("s"))
 
 // 6. Write a program to generate an even random number between 0-20 using `Math.random()`, then log the number.
-
+console.log(Math.floor(Math.random() * 10) + 1)
 
 // Advanced Session Assignments:
 
 // 7. Create a long literal string that spans multiple lines and log it.
+let srt13 = "qwertyuiolkjhgfdsazxcvbnjkijuytredsfghj\
+sdfghjgfdcvbnmjkhgfcv\
+jhgfdcvbbhkjhy\
+jhfvbnm"
+console.log(srt13)
 
 // 8. Write a program that uses escape characters to display the following:
 //    I am learning JavaScript.
@@ -30,20 +42,31 @@ console.log(greeting)
 
 
 // 9. Write a program that declares a variable `amount = 45.85`, then uses `Math.floor()`, `Math.ceil()`, and `Math.round()` on this value and logs each result.
+let amount = 45.85
+console.log("Round method result :" + Math.round(amount))
+console.log("Ceil method result :" + Math.ceil(amount))
+console.log("Floor method result :" + Math.floor(amount))
 
 
 // 10. Create a string `quote = "Learning JavaScript is awesome!"`. Use the `.length` property to find the length of the string and the index of the character 'J'.
-
+let quote = "Learning JavaScript is awesome!"
+console.log("String length :" + quote.length)
+console.log("Indexof og 'J' :" + quote.indexOf("J"))
 
 // 12. Create a string variable `str = 'Hello, World!'` and use an escape character to insert a tab between the words "Hello," and "World!" and log the result.
-
+let str = 'Hello,\t World!'
+console.log("To escape character" + str)
 
 // 13. Given two variables, `str1 = 'abc'` and `str2 = 'def'`, write a program to concatenate these two strings using both the `+` operator and template literals. Log both results.
 let str1 = 'abc';
 let str2 = 'def';
-
+console.log("Concatenation of both strings :" + str1 + str2)
 
 
 // 14. Write a program to pick a random element from string "qwertyuiopasdfghjklzxcvbnm" and log the selected element.
+let str3 = "qwertyuiopasdfghjklzxcvbnm"
+console.log("")
 
 //15 write a program to generate a random odd number form 15-150
+randomOdd = Math.floor(Math.random() * ((150 - 15) / 2 + 1)) * 2 + 15;
+console.log(randomOdd);
--- Batch3_students/Ashwini/Assignments/JS_Assignments/01Assignments/04assignment.js
+++ Batch3_students/Ashwini/Assignments/JS_Assignments/01Assignments/04assignment.js
@@ -2,53 +2,65 @@
 // Simple Questions
 // 1. Find the length of the string "hello world".
 let str1 = "hello world";
-
+console.log("Length of string :"+str1.length)
 
 // 2. Retrieve the last character of the string "automation".
 let str2 = "automation";
+console.log("Last character :"+str2[str2.length-1])
 
 
 // 3. Convert the string "learning" to uppercase.
 let str3 = "learning";
-
+console.log("Upper case :"+str3.toUpperCase())
 
 // 4. Check if the string "coding" includes the word "code".
 let str4 = "coding";
-
+console.log("Include method :"+str4.includes("code"))
 
 // 5. Trim the spaces from the string "   JavaScript   ".
 let str5 = "   JavaScript   ";
+console.log("Trim method :"+str5.trim())
 
 
 // Tough and Advanced Questions
 // 6. Extract the first 5 characters from the string "PlaywrightAutomation".
 let str6 = "PlaywrightAutomation";
+let newstr1 = str6.substr(0, 5)
 
+console.log("Substr method :"+newstr1) 
 
 // 7. Split the string "Cypress is fun" by spaces and print each word on a new line.
 let str7 = "Cypress is fun";
-
+console.log("Split method :"+str7.split(" "))
 
 // 8. Replace  occurrences of 'banana' with 'pineapple' in the string "banana apple".
 let str8 = "banana apple";
-
+console.log("Replace method :"+str8.replace("banana", "pineapple"))
 
 // 9. Use method chaining to convert the string "JavaScript" to uppercase, then lowercase, then uppercase again.
 let str9 = "JavaScript";
+console.log("Chaining method :"+str9.toUpperCase().toLowerCase().toUpperCase())
 
 // 10. Find the character at the 5th position in the string "Functional Testing".
 let str10 = "Functional Testing";
+console.log("Index method :"+str10[5])
 
 // 11. Write a check if  a string and returns true if it includes the word "API",
+let strNo1="API Testing"
+console.log("Include method :"+strNo1.includes("API")) //true
 
 // 12. Extract the last 4 characters from the string "automation testing".
 let str12 = "automation testing";
 
+//let newStr1= substr
+
 // 13. Convert the string "HELLOworld" to lowercase and remove spaces from the beginning and end.
 let str13 = " HELLOworld ";
+console.log("Lowercase :"+str13.toLowerCase())
 
 // 14. Replace the first occurrence of "test" with "exam" in the string "test your code with a test case".
 let str14 = "test your code with a test case";
+console.log("Replace method :"+str14.replace("test", "exam"))
 //thanks 
 
 //-------------------------------------------------------------------------------------------------//

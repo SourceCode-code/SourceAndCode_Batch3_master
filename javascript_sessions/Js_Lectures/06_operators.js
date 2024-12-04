f6e092c9e06293ff1e6fd0f73918984b82f1b07d
KingThomas <akshay.kshirsagar2942@gmaiil.com>
2024-12-04 22:00:15 +0530

    object creation

---
+++ javascript_sessions/Js_Lectures/06_operators.js
@@ -0,0 +1,49 @@
+//Operators--> the symbols used to perfom any action is called operator ---(4-12-24)
+
+//Assignment operator 
+
+//assignment operator --> used to assign  value to any varaiable
+//(=)
+
+let a =5
+//let x =10 , y=20
+//compund assignment operators-->
+//+= --> this adds and assign  x+=y (x=x+y) //30 which is equal to x=x+y
+//-= --> this subrtactrs and assign  x-=y //10 which is equal to  x=x-y
+
+//arthimatic operators--> this operator are used to perfom arthmatic operatorion
+// there is a condition for using arthmatic operators
+
+// arthmatic operation afe performed on  --> to perform arthematic opertor both of the data type should 
+//should be number 
+
+/**
+ * + --> addition
+ * - --> substarction
+ * / --> divide (numerator)
+ * * --> multiplication
+ * % --> modulus  (denominator value )
+ * ** --> exponentiation (power)
+ */
+
+
+console.log(2+3)//5 addition
+console.log(2-3) //1
+console.log(2*3)//6
+console.log(2/3)//0.6
+console.log(2%3)//2
+console.log(2**3)//8
+
+
+
+
+
+
+
+
+
+
+//logical operators
+//comparsion operators
+// per and post increment or decreament operator 
+//ternary operators 

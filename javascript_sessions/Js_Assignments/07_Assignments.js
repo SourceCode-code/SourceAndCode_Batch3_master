f41e8b81c08380c45ca195ddce6bcbbd12c5eb2d
KingThomas <akshay.kshirsagar2942@gmaiil.com>
2024-12-09 21:47:07 +0530

    dates

---
+++ javascript_sessions/Js_Assignments/07_Assignments.js
@@ -0,0 +1,7 @@
+//dates
+//display date in below format
+
+// dd:mm:yyyy HH:mm (24 hours)
+// dd:mm:yyyy HH:mm (12 hours) // (month should be like oct)
+// dd:mm:yyyy HH:mm (12 hours) // (month should be like october)
+// yyyy:mm:DD mm:hh 

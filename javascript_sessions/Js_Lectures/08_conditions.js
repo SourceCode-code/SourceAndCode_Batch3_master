3e51b5b7ea897e3071dd2540e58cc0c615b8f5f2
KingThomas <akshay.kshirsagar2942@gmaiil.com>
2024-12-12 22:09:00 +0530

    conditions

---
+++ javascript_sessions/Js_Lectures/08_conditions.js
@@ -0,0 +1,138 @@
+// when we have to make any decision in js  based on condition
+// conditions -->
+
+//if() --
+//if else --(ternary opertor)
+//else if  --
+//switch cases ---
+
+// if()---> when you have a single condition ton check you use if ()
+
+/**
+ * 
+ * if(condition){
+ * out the will be shown here 
+ * if condition is met
+ * }
+ * 
+ * 
+ */
+
+//--------------------------------------------------------------------------
+
+// check if   the given number is positive interger 
+
+let num = 10
+
+if (num > 0) {
+    console.log(`the given number ${num} is positive interger`)
+}
+
+
+//  example 2 --check if   the given number is positive interger or a negtaive interger
+
+
+// if (num > 0) {
+//     console.log(`the given number ${num} is positive interger`)
+// }
+// if(num<0){
+//     console.log(`the given number ${num} is negative interger`)
+// }
+// // this is not the best the way to check multiple conditions 
+
+
+
+//--------------------------------------------------------
+
+console.log("_______________if else()________________________________")
+
+// when you have check 2 condition then you should use if else()
+// your condition should be either 
+
+let num1 = -50
+
+//num1>0?console.log(`the given number ${num1} is positive interger`): console.log(`the given number ${num1} is negative interger`)
+
+/**
+ * 
+ * 
+ * if(condition){
+ * out the will be shown here 
+ * if condition is met
+ * }
+ * else{
+ * the output if first condition is wrong
+ * }
+ * 
+ * 
+ * 
+ * 
+ */
+if(num1>0){
+    console.log(`the given number ${num1} is positive interger`)
+}
+else{
+    console.log(`the given number ${num1} is negative interger`)  
+}
+
+
+// example 2 
+
+//check if the wehater is raining to carry a umberlla 
+ let israining= false
+
+ if(israining===true){
+    console.log("please carry your raincoats and umberlla's ")
+ }
+ else{
+    console.log("dont carry your rainscoats")
+ }
+
+console.log("_______________else if ()________________________________")
+
+//else if()----------> when you have more than 2 condition to check you use else if ()
+
+//sytnax
+// * if(condition){
+// * out the will be shown here 
+// * if condition is met
+// * }
+
+//elseif (condition 2){
+//output of condition 2
+//}
+
+
+// * else{
+// *default condition
+// * }
+
+//------------------------------------------------
+
+
+// check whetar the number is positive , negative , zero 
+
+let num3 = 0
+
+if(num3>0){
+    console.log(`the given number ${num3} is positive interger`)
+}
+
+else if(num3 < 0){
+    console.log(` the number  ${num3} is negative number`)
+}
+
+else{
+    console.log(` the number  ${num3} is zero`)
+
+
+}
+
+
+
+
+//-------------------------------------------------------------
+//if()----- checks one condition 
+//if else ()--- checks 2 conditions 
+//else if ()--> more than 2 
+

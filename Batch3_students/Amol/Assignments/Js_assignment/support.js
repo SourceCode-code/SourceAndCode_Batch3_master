c1919143404f884fe70992f8baaf8d32a8f0d9bc
Amol <jadhavamol.j2@gmail.com>
2024-12-18 14:06:34 -0500

    Assignment no .09 completed

--- Batch3_students/Amol/Assignments/Js_assignment/support.js
+++ Batch3_students/Amol/Assignments/Js_assignment/support.js
@@ -13,4 +13,12 @@
 // console.log(`${updated}:${Curmin} ${AMPM}`)
 
 // Example: "12/07/2022, 10:24:13 PM"
-    
+let str = " hello i am learning javascript and currently i am seeing the topic loops"
+let STR1 =str.split(" ")
+longestWord =""
+for(let i=1;i<=STR1.length-1;i++){
+ if(STR1[i].length>longestWord.length){
+    longestWord=STR1[i]
+ }
+}
+console.log(longestWord)

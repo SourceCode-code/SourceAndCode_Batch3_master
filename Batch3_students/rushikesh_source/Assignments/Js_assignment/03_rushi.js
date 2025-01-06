522ab31c0b9e2900d6a090b3e19f34067d532a94
rushikesh jadhav <rushikeshoj1199@gmail.com>
2025-01-06 21:52:52 +0530

    04_assignment_complited

--- Batch3_students/rushikesh_source/Assignments/Js_assignment/03_rushi.js
+++ Batch3_students/rushikesh_source/Assignments/Js_assignment/03_rushi.js
@@ -18,11 +18,12 @@
 //console.log(firstName + middleName + lastName)
 
 // 5. Given a string `language = 'JavaScript'`, log the character at the 4th index in the string.
-// let language = 'JavaScript' 
-// console.log(language[4])
+//let language = 'JavaScript' 
+ //console.log(language[4])
 
 // 6. Write a program to generate an even random number between 0-20 using `Math.random()`, then log the number.
-
+//let even = Math.ceil(Math.random())*10
+//console.log(even*2)
 // Advanced Session Assignments:
 
 // 7. Create a long literal string that spans multiple lines and log it.
@@ -31,7 +32,8 @@
 // 8. Write a program that uses escape characters to display the following:
 //    I am learning JavaScript.
 //    It's "fun" to learn JavaScript!
-
+  //let new1 = ( "It\'s \"fun\" to learn JavaScript\! ")
+//console.log(new1)
 
 // 9. Write a program that declares a variable `amount = 45.85`, then uses `Math.floor()`, `Math.ceil()`, and `Math.round()` on this value and logs each result.
 //let amount = "45.85"
@@ -39,8 +41,6 @@
 //console.log(Math.ceil(amount))//46
 //console.log(Math.round(amount))//46
 // 10. Create a string `quote = "Learning JavaScript is awesome!"`. Use the `.length` property to find the length of the string and the index of the character 'J'.
-// let quote = "Learning JavaScript is awesome!"
-
 //console.log(quote.length)//31
 //console.log(quote.indexOf("J"))//9
 // 12. Create a string variable `str = 'Hello, World!'` and use an escape character to insert a tab between the words "Hello," and "World!" and log the result.
--- Batch3_students/rushikesh_source/Assignments/Js_assignment/04_rushi.js
+++ Batch3_students/rushikesh_source/Assignments/Js_assignment/04_rushi.js
@@ -168,5 +168,93 @@
 
 
 
-//----------------------------using for practis------------------------------------------------
+// ---------------------------using for practis------------------------------------------------
+
+
+
+// Simple Session Assignments:
+
+// 1. Declare a string variable `greeting` with the value "Hello World" and log its data type.
+// let greeting = "Hello World"
+// console.log(typeof greeting)
+
+// 2. Declare a number variable `price` with a value of 199.99, then use Math.ceil() to round it up to the nearest whole number and log the result.
+//let price = 199.99
+//console.log(Math.ceil(price))
+
+// 3. Create a string `name` with your full name and log the length of the string using the `.length` property.
+//let fullname = " rushikesh Omprakash Jadhav " 
+//console.log(fullname.length)
+// 4. Write a program that concatenates three string variables `firstName`, `middleName`, and `lastName` using both the `+` operator and template literals, then log the full name.
+//let firstName = "rushikesh"
+//let MiddleName = "omprakash"
+//let lasname = "jadhav"
+//let fullname = ((firstName) + " " + (MiddleName) + " " + (lasname ))
+//console.log(fullname)
+
+
+// 5. Given a string `language = 'JavaScript'`, log the character at the 4th index in the string.
+//let language = 'JavaScript'
+//console.log(language[4] ) 
+
+
+// 6. Write a program to generate an even random number between 0-20 using `Math.random()`, then log the number.
+//console.log(Math.random()*20)
+// let even = (Math.ceil(Math.random()*9)*2)
+// console.log(even)
+
+
+// Advanced Session Assignments:
+
+// 7. Create a long literal string that spans multiple lines and log it.
+//let long = "asfgdhjuikdhcbn\ngfjklo\nstyahi\njfhygn\njhbbggdb\nfrsgv"
+//console.log(long)
+
+
+// 8. Write a program that uses escape characters to display the following:
+//    I am learning JavaScript.
+//    It's "fun" to learn JavaScript!
+// let program = " It\'s \"fun\" to learn JavaScript\! "
+// console.log(program)
+
+
+// 9. Write a program that declares a variable `amount = 45.85`, then uses `Math.floor()`, `Math.ceil()`, and `Math.round()` on this value and logs each result.
+//let amount = 45.85
+//console.log(Math.floor(amount))
+//console.log(Math.ceil(amount))
+//console.log (Math.round(amount))
+
+
+// 10. Create a string `quote = "Learning JavaScript is awesome!"`. Use the `.length` property to find the length of the string and the index of the character 'J'.
+//let quote = "Learning JavaScript is awesome!"
+//console.log(quote.indexOf("J"))
+//console.log(quote.length)
+
+
+// 12. Create a string variable `str = 'Hello, World!'` and use an escape character to insert a tab between the words "Hello," and "World!" and log the result.
+//let str = 'Hello, World!'
+//let str1 = "Hello\tWorld!"
+//console.log(str1)
+
+// 13. Given two variables, `str1 = 'abc'` and `str2 = 'def'`, write a program to concatenate these two strings using both the `+` operator and template literals. Log both results.
+//let str1 = 'abc';
+//let str2 = 'def';
+//let str1 = 'abc'
+//let str2 = 'def'
+//console.log(str1 + str2)
+
+// 14. Write a program to pick a random element from string "qwertyuiopasdfghjklzxcvbnm" and log the selected element.
+
+//15 write a program to generate a random odd number form 15-150
+
+
+
+
+// ------------------------------test-----------------------
+
+const str27= "jadhav"
+
+console.log(str27.substring(1,4))
+//console.log(str27.endsWith("t"))
+
 

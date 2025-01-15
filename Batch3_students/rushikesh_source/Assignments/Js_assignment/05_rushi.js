34f5d28b1aab3e934408bec23dbadb0cebc86378
rushikesh jadhav <rushikeshoj1199@gmail.com>
2025-01-15 05:54:23 +0530

    practisc_for_functions

---
+++ Batch3_students/rushikesh_source/Assignments/Js_assignment/05_rushi.js
@@ -0,0 +1,81 @@
+
+
+// 1 function without parameter without retrun type 
+function fullname() {
+    console.log("siddhant arjun gadakh")
+}
+fullname()
+
+           // EX 1 
+ function schoolename(){
+    console.log("nutan school")
+    console.log(schoolename.length)
+    console.log(typeof schoolename)
+ }
+ schoolename()
+
+// EX2
+ function phonenum(){
+    console.log(8237071005)
+ }
+ phonenum()
+
+ // EX3
+function age(){
+ console.log(25)
+} 
+  age()
+
+//   EX 4
+function hight(){
+    console.log(5.7)
+}
+ hight() 
+
+
+//--------------------2 function  with parameter without retrun type----------------------------- 
+
+function myfullname(name) {
+    console.log(name)
+}
+myfullname("siddhant ")
+myfullname("vaibhav")
+ 
+//           EX 1 
+function myschoolname(name){
+    
+    console.log(name)
+}
+myschoolname("nutan school")
+myschoolname(123456)
+myschoolname()
+
+//            EX2
+function myphonenum(num){
+    console.log(num)
+}
+myphonenum(124567)
+myphonenum('rushikesh')
+myphonenum(20-15)
+
+//            EX3
+function myage(age){
+console.log(age)
+}
+myage(25)
+myage(-43)
+myage("rushi")
+
+//            EX4
+function myhight(hight){
+    console.log(hight)
+}
+myhight(2.5)
+myhight(-43)
+myhight(5.676)
+
+// -----------------/ 3 function with paramter and with retrun type------------------------------------------
+
+// retrun type -- > 
+
+
---
+++ Batch3_students/rushikesh_source/Interview/one.js
@@ -0,0 +1,17 @@
+let num = 2
+for (let i = 1; i <= 10; i++) {
+    console.log(`${num} x ${i}  == ${num * i}`)
+}
+
+// print the sqaure of each number untill 25
+
+let num1 = 2
+for (let i = 1; i<= 5; i++){
+    console.log(` ${i * i}`)
+}
+// print the even numbers till 10 
+
+for (let i = 2; i<= 10; i++){
+   
+    console.log(i)
+}

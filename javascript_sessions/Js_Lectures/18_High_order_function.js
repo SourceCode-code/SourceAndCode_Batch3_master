fdeb67bf637f50245b22ddc61243de70c7d242d9
KingThomas <akshay.kshirsagar2942@gmaiil.com>
2025-02-10 21:38:35 +0530

    high order function

--- javascript_sessions/Js_Lectures/18_High_order_function.js
+++ javascript_sessions/Js_Lectures/18_High_order_function.js
@@ -3,8 +3,8 @@
 
 // create a function that will give number of count of voules in the string 
 
-function countvowel(string){
-console.log(string.match(/[AEIOU]/gi).length)
+function countvowel(string) {
+    console.log(string.match(/[AEIOU]/gi).length)
 }
 
 
@@ -30,19 +30,92 @@ countvowel("Siddhant")
  * 
  */
 
-const  callback = (n)=>{
-    return n**2
+const callback = (n) => {
+    return n ** 2
 }
 
 
-function cube (callback,n){
-    return callback(n)*n
+function cube(callback, n) {
+    return callback(n) * n
 }
 
 
-console.log(cube(callback,3))
+console.log(cube(callback, 3))
 
 
-// return
-//setinterval 
-//settimeout
+// return --> when one function return another function it is called as retruring 
+/**
+ * when out put of one function is another function 
+ * 
+ * 
+ */
+
+// const higherorder = n => {
+//     const dosomething = m => {
+//         const dowhatever = t => {
+//             return 2 * n + 3 * m * t
+
+//         }
+//         return dowhatever
+//     }
+//     return dosomething
+// }
+
+// console.log(higherorder(2)(3)(4))
+
+
+// //setinterval --> if you want a output at a certain interval 
+// // example if you want a function to call it self after a certain 
+
+
+
+// // function sayHello(){
+// //     console.log("hello")
+// // }
+
+// // setInterval(sayHello,3000)
+
+
+// // -- output after every 3 sec hello print 
+
+
+// //settimeout --> a function which delay the out by given a certain 
+// console.log("hello") // 
+// function bye() {
+//     console.log("goodbye")
+
+// }
+
+
+// setTimeout(bye, 5000)
+
+// // console.log("thank you")
+
+// function th() {
+//     console.log("thank you")
+
+
+// }
+
+// setTimeout(th, 6000)
+
+
+// json
+// object creation
+//sync and async (promsies and callback hell)
+// intro to oops 
+
+
+
+// setInterval -- setTimeout  -->  advance types of callback functions
+
+
+let interval = setInterval(() => {
+    console.log("this will repeat after 2 sec")
+}, 2000)
+
+
+setTimeout(() => {
+    clearInterval(interval)
+    console.log("the message has ended")
+}, 6000)

c9194de3b878d54c16cd9fdc137cbe85d6b44202
Ashwini Tanaji Kadam <ashwinit.kadam@expleogroup.com>
2025-02-11 19:19:00 +0530

    practice

--- Batch3_students/Ashwini/Assignments/JS_Assignments/01Assignments/17assignment.js
+++ Batch3_students/Ashwini/Assignments/JS_Assignments/01Assignments/17assignment.js
@@ -19,9 +19,17 @@
 // Exercises: Level 2
 
 // 1 Write a function called tenMostFrequentWords which get the ten most frequent word from a string?
-
-//     paragraph = `I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.`
-//     console.log(tenMostFrequentWords(paragraph))
+/*let paragraph = `I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.`
+String word[] = 0
+String count[] = 0
+function tenMostFrequentWords() {
+    for (let i = 0; i < paragraph.length; i++) {
+        word[i].push();
+        console.log(word[i], count[i])
+    }
+}
+console.log(tenMostFrequentWords(paragraph))
+*/
 //     [
 //     {word:'love', count:6},
 //     {word:'you', count:5},
@@ -60,10 +68,14 @@
 // Exercises: Level 3
 
 // 1. Write a function which cleans text. Clean the following text. After cleaning, count three most frequent words in the string.
-//   sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`
-//   console.log(cleanText(sentence))
+let sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`
+// console.log(cleanText(sentence))
+ console.log(sentence.replaceAll(/%$@#&!?;/gim, " "))
+
 //  I am a teacher and I love teaching There is nothing as more rewarding as educating and empowering people I found teaching more interesting than any other jobs Does this motivate you to be a teacher
 //  ```
+
+
 // 2. Write a function which find the most frequent words. After cleaning, count three most frequent words in the string.
 
 // ```js

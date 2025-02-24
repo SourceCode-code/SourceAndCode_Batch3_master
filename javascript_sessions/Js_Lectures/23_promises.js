f8b1181e4752caf9a9e1d4e07636d58e1389486e
KingThomas <akshay.kshirsagar2942@gmaiil.com>
2025-02-24 21:37:19 +0530

    promises

---
+++ javascript_sessions/Js_Lectures/23_promises.js
@@ -0,0 +1,58 @@
+// promises --> (cypress )
+
+// promises are the way to reslove async code in sync 
+
+// promises --> commitment
+
+//1 fullfilled --  success
+//2 rejected --  failure 
+//3 pending -- 
+
+
+// pending --> intial stage (not started )
+// fulfiiled --> operation has been successfully performed 
+// rejection --> operation has failed 
+
+//----------------------------------------------------
+
+// example of promises 
+
+//write a simple promises -->
+
+let pro = new Promise((reslove, rejected) => {
+    let success = true
+
+    if (success === true) {
+        reslove("the operation has been successfull")
+    }
+    else {
+        rejected("error try again")
+    }
+}).then((message) => {
+    console.log(message)
+}).catch((error) => {
+    console.log(error)
+})
+
+//----------------------------------------------------------
+
+let skills = new Promise((reslove, reject) => {
+    setTimeout(() => {
+        let arr = ["html", "css", "js", "cy"]
+        if (arr.length > 1) {
+            reslove(arr)
+        }
+        else{
+            reject("person doesnt meet critery")
+        }
+    }, 3000)
+})
+
+skills.then((message)=>{
+    console.log(message)
+}).catch((error)=>{
+    console.log(error)
+})
+
+
+//--------------------------------------------------------------------

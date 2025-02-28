6c7e3071ee005748fb027ebf2f86d223e87a474f
SonuKhosle <sonukhosle@gmail.com>
2025-02-28 15:08:34 +0530

    promises practice

--- Batch3_students/Sonali/Notes/prmises_eg.js
+++ Batch3_students/Sonali/Notes/prmises_eg.js
@@ -16,3 +16,52 @@ else{
     .catch((error)=>{
         console.log(error)
 })
+
+// //1 user regsister
+// //2 otp verification
+// //3 perfrence --
+// //4 start acitivity
+
+
+function user_registration(user){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+console.log(`${user} registered successfully`)
+resolve(user)
+        },700)
+    })
+}
+
+
+
+function otp_verify(user){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log(`${user} otp verified successfully`)
+            resolve(user)
+        },500)
+    })
+}
+
+function prefences(user){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log(`${user} will set prefences`)
+            resolve(user)
+        },1000)
+    })
+}
+
+function start_Activity(user){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log(`${user} will start the activity`)
+            resolve(user)
+        },2000)
+    })
+}
+
+user_registration("SONALI")
+.then(otp_verify)
+.then(prefences)
+.then(start_Activity)

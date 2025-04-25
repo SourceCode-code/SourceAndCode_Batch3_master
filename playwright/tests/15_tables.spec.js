78ede5b6e2a0ac0ee8741a325d99d2fb85f48128
KingThomas <akshay.kshirsagar2942@gmaiil.com>
2025-04-25 21:31:03 +0530

    tables

--- playwright/tests/15_tables.spec.js
+++ playwright/tests/15_tables.spec.js
@@ -1,23 +1,18 @@
-const{test,expect}=require("@playwright/test")
+const { test, expect } = require("@playwright/test")
 
 
 
-test("verify tables ",async({page})=>{
+test("verify tables ", async ({ page }) => {
 
+    await page.goto("https://webdriveruniversity.com/Data-Table/index.html")
+    let firstnames = await page.locator('[id="t01"] tbody tr td:nth-child(1)')
+    let age = await page.locator('[id="t01"]>tbody>tr>td:nth-child(3)')
+    let endcount = await page.locator('[id="t01"] tbody tr td:nth-child(1)').count()
 
-await page.goto("https://webdriveruniversity.com/Data-Table/index.html")
-let firstnames = await page.locator('[id="t01"] tbody tr td:nth-child(1)')
-let age = await page.locator('[id="t01"]>tbody>tr>td:nth-child(3)')
-let endcount = await page.locator('[id="t01"] tbody tr td:nth-child(1)').count()
-
-for(let i =0;i<endcount;i++){
-    let fname = await firstnames.nth(i).textContent()
-    let agge = await age.nth(i).textContent()
-    console.log(`${fname} age is ${agge}`)
-}
-
-
-
-
+    for (let i = 0; i < endcount; i++) {
+        let fname = await firstnames.nth(i).textContent()
+        let agge = await age.nth(i).textContent()
+        console.log(`${fname} age is ${agge}`)
+    }
 })
 

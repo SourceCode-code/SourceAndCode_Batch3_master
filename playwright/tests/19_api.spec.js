b1e320e158f3ef48961c95e123cd1360824e391b
KingThomas <akshay.kshirsagar2942@gmaiil.com>
2025-05-08 21:04:33 +0530

    api theroy

---
+++ playwright/tests/19_api.spec.js
@@ -0,0 +1,53 @@
+//API --> Application programing interface
+
+//API--> api is a communication link between front end and backend
+
+//website  --->
+//front end -->the code which is used to build the ui of the webviste and is visible in dom  --> (html , css , js , react, anugalr )
+//backend   --> the code which is used to bulit the server effincey server logic --> (.net , python,java )
+//server   --->(sql , mongodb--> databse)
+//devops -->
+
+// it techincal roles
+
+// front end developer
+// backend developer
+//full stack developer
+// mannual testor
+// api testor
+// database engineer
+// automation testor
+//devops
+
+//----------------------------------------------------------
+//example
+
+//customer --> order something to eat  ||         (user perform some action on front )
+//chef --> prepare the actual order   ||           (the action is done in back end )
+//waiter -->communication link          ||         (api is the communication link )
+
+//API -->
+
+//there are two type of services
+
+// SOAP -->xml (the api carries data in xml format) --(this is an old service )
+// REST -->json (the carries data in json format) --(this is a new service )
+
+
+// testing tools -->
+//SOAP -- SOAP UI
+// REST --> POSTMAN, SWAGGER
+
+
+//REST-->
+
+//GET  --> it gets all the data
+//POST --> Create new data
+//PUT  --> update created data
+//UPDATE-->update created data
+//PATCH-->update created data
+//DELETE --> delete the data 
+
+
+// when you test single api --> api request
+// multiple apis (cllection api )--> collection 
